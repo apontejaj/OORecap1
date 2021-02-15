87cd89994410fea9dabe2717b98e0210fd935222
Amilcar Aponte <apontejaj@gmail.com>
2021-02-15 16:19:20 +0000

    V5

--- src/oorecap1/OORecap1.java
+++ src/oorecap1/OORecap1.java
@@ -9,7 +9,6 @@ import model.Car;
 import model.Dog;
 import model.Person;
 
-
 /**
  *
  * @author apont
@@ -21,26 +20,26 @@ public class OORecap1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+
         Person p1 = new Person();
-        
+
         Person p2 = new Person(25, "Male", "Diego");
-        
+
         System.out.println(p1);
         System.out.println(p2);
-        
-        
+
         Car c1 = new Car();
-        
+
         System.out.println(c1.getMake());
-        
+
         Dog d1 = new Dog("Fluffy");
         d1.setOwner(p2);
-        
-        System.out.println(d1);
-        
 
-        
+        System.out.println(d1);
+
+        Car c2 = new Car();
+        Car c4 = new Car();
+
     }
-    
+
 }
